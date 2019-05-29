4859dfc56fb4f0420f6dfce582312e670b687dba
Suspekain <suspekain@gmail.com>
2019-05-29 10:45:23 +0200

    a

---
+++ src/NonEditableTableModel.java
@@ -0,0 +1,11 @@
+package src;
+import javax.swing.table.DefaultTableModel;
+
+public class NonEditableTableModel extends DefaultTableModel {
+
+    	
+    	public boolean isCellEditable(final int rowIndex, final int columnIndex)
+    	{
+    		return false;
+    	}
+    }
